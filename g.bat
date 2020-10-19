c76767c65859a35fab60e218adacb858d64c4d1f
Fzl <f18970254621@163.com>
2020-10-19 17:13:02 +0800

    123

---
+++ g.bat
@@ -0,0 +1,10 @@
+:: show git status
+git status
+:: add all changing
+git add -A
+:: local commit
+git commit -m "123"
+:: show git pull
+git pull origin master --no-edit
+:: push to remote repository
+git push origin master:master
